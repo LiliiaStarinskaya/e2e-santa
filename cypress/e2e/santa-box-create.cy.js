3c8fa3d2c74a384a758a4ce4b405105b064520b1
Lstarinskaya <starinskayaliliia@gmail.com>
2024-04-13 05:13:25 +0300

    Added updated test suite for 6.1.1

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -3,27 +3,18 @@ const boxPage = require("../fixtures/pages/boxPage.json");
 const generalElements = require("../fixtures/pages/general.json");
 const dashboardPage = require("../fixtures/pages/dashboardPage.json");
 const invitePage = require("../fixtures/pages/invitePage.json");
-const inviteeBoxPage = require("../fixtures/pages/inviteeBoxPage.json");
 const inviteeDashboardPage = require("../fixtures/pages/inviteeDashboardPage.json");
+const lotteryPage = require("../fixtures/pages/lottery.json");
 import { faker } from "@faker-js/faker";
 
 describe("user can create a box and run it", () => {
-  //пользователь 1 логинится
-  //пользователь 1 создает коробку
-  //пользователь 1 получает приглашение
-  //пользователь 2 переходит по приглашению
-  //пользователь 2 заполняет анкету
-  //пользователь 3 переходит по приглашению
-  //пользователь 3 заполняет анкету
-  //пользователь 4 переходит по приглашению
-  //пользователь 4 заполняет анкету
-  //пользователь 1 логинится
-  //пользователь 1 запускает жеребьевку
   let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
-  let wishes = faker.word.noun() + faker.word.adverb() + faker.word.adjective();
+  let userWishes =
+    faker.word.noun() + faker.word.adverb() + faker.word.adjective();
   let maxAmount = 50;
   let currency = "Евро";
   let inviteLink;
+  let boxId;
 
   it("user logins and create a box", () => {
     cy.visit("/login");
@@ -34,23 +25,30 @@ describe("user can create a box and run it", () => {
     cy.get(boxPage.sixthIcon).click();
     cy.get(generalElements.arrowRight).click();
     cy.get(boxPage.giftPriceToggle).check({ force: true });
-    cy.get(boxPage.maxAnount).type(maxAmount);
+    cy.get(boxPage.maxAmount).type(maxAmount);
     cy.get(boxPage.currency).select(currency);
+    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({ force: true });
     cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
+
     cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
-    cy.get(".layout-1__header-wrapper-fixed .toggle-menu-item span")
+    cy.get(generalElements.boxMenu)
       .invoke("text")
       .then((text) => {
         expect(text).to.include("Участники");
         expect(text).to.include("Моя карточка");
         expect(text).to.include("Подопечный");
       });
+
+    cy.url().then((url) => {
+      const parts = url.split("/");
+      boxId = parts[parts.length - 1];
+      cy.log("Box ID:", boxId);
+    });
   });
 
-  it("add participants", () => {
-    cy.get(generalElements.submitButton).click();
+  it("add participants via link", () => {
+    cy.get(generalElements.submitButton).click({ force: true });
     cy.get(invitePage.inviteLink)
       .invoke("text")
       .then((link) => {
@@ -58,17 +56,28 @@ describe("user can create a box and run it", () => {
       });
     cy.clearCookies();
   });
+
+  it("add participants manually", () => {
+    cy.get(invitePage.inviteeNameFieldFirst).type(users.user1.name);
+    cy.get(invitePage.inviteeEmailFieldFirst).type(users.user1.email);
+    cy.get(invitePage.inviteeNameFieldSecond).type(users.user2.name);
+    cy.get(invitePage.inviteeEmailFieldSecond).type(users.user2.email);
+    cy.get(invitePage.inviteeNameFieldThird).type(users.user3.name);
+    cy.get(invitePage.inviteeEmailFieldThird).type(users.user3.email);
+    cy.get(invitePage.button).click({ force: true });
+    cy.get(invitePage.messageField)
+      .invoke("text")
+      .should(
+        "include",
+        "Карточки участников успешно созданы и приглашения уже отправляются."
+      );
+
+    cy.clearCookies();
+  });
+
   it("approve as user1", () => {
-    cy.visit(inviteLink);
-    cy.get(generalElements.submitButton).click();
-    cy.contains("войдите").click();
-    cy.login(users.user1.email, users.user1.password);
-    cy.contains("Создать карточку участника").should("exist");
-    cy.get(generalElements.submitButton).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(generalElements.arrowRight).click();
-    cy.get(inviteeBoxPage.wishesInput).type(wishes);
-    cy.get(generalElements.arrowRight).click();
+    cy.approveUser1(inviteLink, users);
+    cy.createUserCard(userWishes);
     cy.get(inviteeDashboardPage.noticeForInvitee)
       .invoke("text")
       .then((text) => {
@@ -77,20 +86,54 @@ describe("user can create a box and run it", () => {
     cy.clearCookies();
   });
 
-  after("delete box", () => {
+  it("approve as user2", () => {
+    cy.approveUser2(inviteLink, users);
+    cy.createUserCard(userWishes);
+    cy.get(inviteeDashboardPage.noticeForInvitee)
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.contain("Это — анонимный чат с вашим Тайным Сантой");
+      });
+    cy.clearCookies();
+  });
+
+  it("approve as user3", () => {
+    cy.approveUser3(inviteLink, users);
+    cy.createUserCard(userWishes);
+    cy.get(inviteeDashboardPage.noticeForInvitee)
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.contain("Это — анонимный чат с вашим Тайным Сантой");
+      });
+    cy.clearCookies();
+  });
+
+  it("lottery", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
-    cy.get(
-      '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-    ).click();
-    cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
-    cy.get(
-      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-    ).click();
-    cy.contains("Архивация и удаление").click({ force: true });
-    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
-      "Удалить коробку"
-    );
-    cy.get(".btn-service").click();
+    cy.get(generalElements.boxesButton).click();
+    cy.contains(newBoxName).click({ force: true });
+    cy.get(lotteryPage.lotterySubmitLink).click({ force: true });
+    cy.get(lotteryPage.lotterySubmitButton).click();
+    cy.get(lotteryPage.secondSubmitButton).click();
+    cy.get(lotteryPage.notice)
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.contain("Жеребьевка проведена");
+      });
+    cy.clearCookies();
+  });
+
+  it("Delete_box", () => {
+    cy.request({
+      method: "DELETE",
+      headers: {
+        Cookie:
+          "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY1NTI1NjUsImlhdCI6MTcxMjk1NzY1MiwiZXhwIjoxNzE1NTQ5NjUyfQ.ISTy4N-ZRB04PQ0Di_0VGjaVJ5xlWYQy0EdVo-ubE3M",
+      },
+      url: "https://santa-secret.ru/api/box/" + boxId,
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
   });
 });

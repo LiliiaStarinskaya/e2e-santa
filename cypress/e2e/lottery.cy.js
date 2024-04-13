9e8aa406e5cc9e40ae5104e1e1407249f4c4da2c
Lstarinskaya <starinskayaliliia@gmail.com>
2024-04-14 00:26:07 +0300

    Updated lottery script for 6.1.1

--- cypress/e2e/lottery.cy.js
+++ cypress/e2e/lottery.cy.js
@@ -1,23 +1,26 @@
 const users = require("../fixtures/users.json");
 const generalElements = require("../fixtures/pages/general.json");
 const lotteryPage = require("../fixtures/pages/lottery.json");
-import { faker } from "@faker-js/faker";
+const boxPage = require("../fixtures/pages/boxPage.json");
 
 describe("user can perform a lottery", () => {
-  let newBoxName = faker.word.noun({ length: { min: 5, max: 10 } });
   it("lottery", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
     cy.get(generalElements.boxesButton).click();
-    cy.contains(newBoxName).click({ force: true });
-    cy.get(lotteryPage.lotterySubmitLink).click({ force: true });
-    cy.get(lotteryPage.lotterySubmitButton).click();
+    cy.get(boxPage.boxElement).click();
+    cy.get(generalElements.gearIcon).click();
+    cy.get(lotteryPage.lotterySidebarButton).click({ forse: true });
+    cy.get(generalElements.submitButton).click();
     cy.get(lotteryPage.secondSubmitButton).click();
+
     cy.get(lotteryPage.notice)
       .invoke("text")
       .then((text) => {
         expect(text).to.contain("Жеребьевка проведена");
       });
+    cy.get(generalElements.gearIcon).click();
+    cy.contains("Проведена").should("exist");
     cy.clearCookies();
   });
 });
